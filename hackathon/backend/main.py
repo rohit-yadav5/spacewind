4a3dbb13d25de8f6f601505ae63c4db2bef88826
Rohit Yadav <rohityadav.0620@gmail.com>
2025-09-26 01:08:37 +0530

    hackathon setup

--- hackathon/backend/main.py
+++ hackathon/backend/main.py
@@ -9,4 +9,4 @@ frontend_path = os.path.join(os.path.dirname(__file__), '../frontend')
 app.mount("/", StaticFiles(directory=frontend_path, html=True), name="static")
 
 if __name__ == "__main__":
-	uvicorn.run("main:app", host="0.0.0.0", port=8600, reload=True)
+	uvicorn.run("main:app", host="0.0.0.0", port=8700, reload=True)
